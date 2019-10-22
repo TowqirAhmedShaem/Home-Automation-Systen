30b344535e1c90a60487566d3732ce75a2bc17bd
Shaem <towqirahmedshaem@gmail.com>
2019-10-22 13:19:27 +0600

    test

--- Code/homeAutomationSystem/homeAutomationSystem.ino
+++ Code/homeAutomationSystem/homeAutomationSystem.ino
@@ -1,7 +1,12 @@
 #define buzzer 11
 #define ldrPin A0
+#define tempPin A1
 #define flame 8
 int Flame = HIGH;
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
 
 void setup() {
   Serial.begin(9600);
@@ -10,31 +15,52 @@ void setup() {
   pinMode(ldrPin, INPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(flame, INPUT);
+  pinMode(tempPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  //  Flame = digitalRead(flame);
-  //  if (Flame == LOW) {
-  //    digitalWrite(buzzer, HIGH);
-  //    delay(200);
-  //  }
-  //  else {
-  //    digitalWrite(buzzer, LOW);
-  //  }
+  //Flame Start
+  Flame = digitalRead(flame);
+  if (Flame == LOW) {
+    //    digitalWrite(buzzer, HIGH);
+    Serial.println("Fire Detected");
+    delay(200);
+  }
+  else {
+    digitalWrite(buzzer, LOW);
+  }
+  //Flame End
 
+  // LDR
   int ldrStatus = analogRead(ldrPin);
-  Serial.print("LDR Value: ");
-  Serial.println(ldrStatus);
+  // Serial.print("LDR Value: ");
+  //Serial.println(ldrStatus);
 
   if (ldrStatus <= 900) {
-    Serial.println("No Light Detected");
-    digitalWrite(buzzer, LOW);
+    //Serial.println("No Light Detected");
+    //digitalWrite(buzzer, LOW);
 
   }
 
   else {
     Serial.println("Light Detected");
-    digitalWrite(buzzer, HIGH);
+    //digitalWrite(buzzer, HIGH);
   }
   delay(500);
+  //ldr end
+
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+Serial.print("Distance: ");
+Serial.println(distance);
+
+  Serial.println("**********");
 }
